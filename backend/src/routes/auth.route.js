0a8813b8d17867222781ab7e3c3e0f24d5fe1e85
Aditya Raj <aditya41207@gmail.com>
2025-10-11 12:48:25 +0530

    rate-limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const router =express.Router();
 
 
+router.use(arcjetProtection);
+router.post("/signup", signup);
 
 
-router.post("/signup",signup);
-
-
-router.post("/login", login);
+router.post("/login",login);
 
 
 router.post("/logout", logout);
